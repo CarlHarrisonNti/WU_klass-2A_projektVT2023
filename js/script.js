e2845a6f5aef3f79cc92a3972cff97de9fd97339
meatball <meatballsofmeatballs@gmail.com>
2023-02-02 11:53:28 +0100

    jobbat på language menyn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.meny');
+var hamburger = document.querySelector('.hamburger');
+var menu = document.querySelector('.meny');
+var python = document.querySelector('#python');
 hamburger === null || hamburger === void 0 ? void 0 : hamburger.addEventListener('click', function () {
     hamburger === null || hamburger === void 0 ? void 0 : hamburger.classList.toggle('open');
     menu === null || menu === void 0 ? void 0 : menu.classList.toggle('open');
+});
+python === null || python === void 0 ? void 0 : python.addEventListener('click', function () {
+    python === null || python === void 0 ? void 0 : python.classList.toggle('open');
 });
--- ts/script.ts
+++ ts/script.ts
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.meny');
+const python = document.querySelector('#python');
 
 hamburger?.addEventListener('click', () => {
   hamburger?.classList.toggle('open');
   menu?.classList.toggle('open');
+});
+
+python?.addEventListener('click', () => {
+  python?.classList.toggle('open');
 });
