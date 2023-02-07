732df477fa7a60b0a9da39cba4383d771f039f79
meatball <meatballsofmeatballs@gmail.com>
2023-02-07 08:57:14 +0100

    Lagt till mycket content

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 var hamburger = document.querySelector('.hamburger');
 var menu = document.querySelector('.meny');
 var python = document.querySelector('#python');
+var ruby = document.querySelector('#ruby');
+var elixir = document.querySelector('#elixir');
 hamburger === null || hamburger === void 0 ? void 0 : hamburger.addEventListener('click', function () {
     hamburger === null || hamburger === void 0 ? void 0 : hamburger.classList.toggle('open');
     menu === null || menu === void 0 ? void 0 : menu.classList.toggle('open');
 });
 python === null || python === void 0 ? void 0 : python.addEventListener('click', function () {
     python === null || python === void 0 ? void 0 : python.classList.toggle('open');
+});
+ruby === null || ruby === void 0 ? void 0 : ruby.addEventListener('click', function () {
+    ruby === null || ruby === void 0 ? void 0 : ruby.classList.toggle('open');
+});
+
+elixir === null || elixir === void 0 ? void 0 : elixir.addEventListener('click', function () {
+    elixir === null || elixir === void 0 ? void 0 : elixir.classList.toggle('open');
 });
--- ts/script.ts
+++ ts/script.ts
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.meny');
 const python = document.querySelector('#python');
+const ruby = document.querySelector('#ruby');
+const elixir = document.querySelector('#elixir');
 
 hamburger?.addEventListener('click', () => {
   hamburger?.classList.toggle('open');
@@ -9,4 +11,12 @@ hamburger?.addEventListener('click', () => {
 
 python?.addEventListener('click', () => {
   python?.classList.toggle('open');
+});
+
+ruby?.addEventListener('click', () => {
+  ruby?.classList.toggle('open');
+});
+
+elixir?.addEventListener('click', () => {
+  elixir?.classList.toggle('open');
 });
